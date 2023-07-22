e85550338462d0235f015b3a4d858c3d5c5e7164
ILYESS LASS <lassbnali@gmail.com>
2023-07-22 05:27:42 +0100

    Initial commit

---
+++ src/main/java/com/cuisine/macuisine/MacuisineApplication.java
@@ -0,0 +1,13 @@
+package com.cuisine.macuisine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacuisineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MacuisineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cuisine/macuisine/MacuisineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuisine.macuisine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacuisineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
